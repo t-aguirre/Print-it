2127f1715b28676367119e6fee3fe6aa210324fa
mayi <tala.aguirre@gmail.com>
2024-10-04 17:08:47 +0200

    Feat(header):Implement looping slider with next/previous arrows and active bullet indication on each load

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,21 @@
-# Print-It WebSite v.1
+<p align="left">
+  <img src="assets/images/logo.png" alt="logo The Artbox" height="19px">
+</p>
+
+# Print it! is a small family-owned print shop 
+
+The owners seek to make its website header dynamic with a **full-width carousel**.
+
+## 🔹 Dynamic Slider Features
+
+This project includes a dynamic slider implemented with several key features to enhance the user experience. The slider showcases a full-width carousel that rotates through a series of slides, each with its own image and tagline. 
+
+The functionality includes:
+
+**Slide navigation**: Users can navigate between slides using left and right arrows. Clicking the left arrow decrements the slide index, cycling back to the last slide when the beginning is reached. Conversely, clicking the right arrow increments the slide index, cycling to the first slide when the end is surpassed.
+
+**Automatic slide transition**: The slider features an automatic transition that changes slides every 3 seconds, providing a seamless viewing experience without manual interaction.
+
+**Active bullet indicator**: The slider includes visual indicators at the bottom, which highlight the current slide. As the slide changes, the corresponding bullet change it style, indicating the active slide and ensuring users are aware of their current position in the carousel.
+
+These features combine to create an engaging and interactive carousel that dynamically updates based on user input and time intervals.
--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//SLIDER
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -16,3 +17,75 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// Variable to track current slide index
+let currentSlideIndex = 0;
+
+// DOM elements to be updated - images and taglines
+const sliderImage = document.querySelector("#banner .banner-img")
+const sliderTagline = document.querySelector("#banner p");
+
+// Selecting bullets div
+const dots = document.querySelector("#banner .dots");
+
+//Adding as bullets div to the DOM as images on the slider
+slides.forEach((slide, index) =>  {
+	const dot = document.createElement('div');
+	dot.classList.add('dot');
+	dot.dataset.index = index //storing the index
+	dots.appendChild(dot);
+});
+
+//Function to update the image, tagline and active bullet
+function updateSlide(index) {
+	sliderImage.src = `./assets/images/slideshow/${slides[index].image}`;
+	sliderTagline.innerHTML = slides[index].tagLine;
+	const allDots = document.querySelectorAll('#banner .dots .dot');
+	allDots.forEach(dot => dot.classList.remove('dot_selected'));
+	allDots[index].classList.add('dot_selected');
+	}
+
+//Slider arrows
+function goToPreviousSlide() {
+	//Decrement the slide index
+	currentSlideIndex--;
+	if (currentSlideIndex < 0) {
+		currentSlideIndex = slides.length -1;
+	}
+	//Updating the displayed slide
+	updateSlide(currentSlideIndex);
+}
+
+const arrowLeft = document.querySelector("#banner .arrow_left");
+arrowLeft.addEventListener('click', goToPreviousSlide);
+
+
+function goToNextslide () {
+	currentSlideIndex++;
+	if (currentSlideIndex >= slides.length) {
+		currentSlideIndex = 0;
+	}
+	updateSlide (currentSlideIndex);
+}
+
+const arrowRight = document.querySelector("#banner .arrow_right");
+arrowRight.addEventListener('click', goToNextslide);
+
+// Automatic slideshow
+let autoSlideInterval = setInterval(goToNextslide, 3000);
+
+// On click on each bullet,the correct image is set
+const allDots = document.querySelectorAll('#banner .dots .dot');
+allDots.forEach((dot, index) => {
+	dot.addEventListener('click', function() {
+		currentSlideIndex = index;
+		updateSlide(currentSlideIndex);
+	});
+});
+
+
+
+
+
+
+
